ca60d131fed798ccaa93e3fafd5e2cdd591a2ecf
KIMHONG SUN <sun.kimhong19@kit.edu.kh>
2021-11-26 23:21:35 +0700

    complete homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import Particles from 'react-particles-js';
+import Header from './components/Header';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
+    <Particles
+        style={{ position: "absolute" }}
+        height="95%"
+        width="95%"
+        params={{
+          particles: {
+            color: {
+              value: "#000000"
+            },
+            line_linked: {
+              color: {
+                value: "#000000"
+              }
+            },
+            number: {
+              value: 50
+            },
+            size: {
+              value: 3
+            }
+          }
+        }}
+      />
     <Navbar/>
+    <Header/>
+    </>
   );
 }
 
